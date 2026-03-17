511513b97473e917a0e41f5b7ffcf90e3c8af799
czf <5413699@qq.com>
2026-03-17 21:18:24 +0800

    记录了一批实验结果

---
+++ result_exhibition.m
@@ -0,0 +1,11 @@
+%[text] ## 实验结果展示
+%[text] 可运行此代码，查看论文相关的结果图表
+%[text] 1\.四种方法下阻塞率随服务到达率的变化情况
+%[text] 源数据：c.输出/论文数据.xlsx中的阻塞率sheet
+
+
+%[appendix]{"version":"1.0"}
+%---
+%[metadata:view]
+%   data: {"layout":"onright"}
+%---
